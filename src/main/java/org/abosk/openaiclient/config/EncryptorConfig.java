67de6ba08b38c21f869def3f96e4924d4814c9ee
bosc039 <arnau.bosch@arvato-scs.com>
2025-05-31 22:25:12 +0200

    Configure Jasypt encryptor to use password from application properties
    It is mandatory to set the password for jasypt via VM.

--- src/main/java/org/abosk/openaiclient/config/EncryptorConfig.java
+++ src/main/java/org/abosk/openaiclient/config/EncryptorConfig.java
@@ -2,15 +2,20 @@ package org.abosk.openaiclient.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EncryptorConfig {
 
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(password);
         encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
         encryptor.setStringOutputType("base64");
         return encryptor;
